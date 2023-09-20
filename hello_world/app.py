8929608b0eecbe0de68943248ce263dc8688c4a8
Evan Haston <ehaston@darkmatterit.io>
2023-09-20 18:32:56 -0400

    feature: adding a welcome to the group

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello  All welcome to the AWS User Group Meetup in Connneticut",
             # "location": ip.text.replace("\n", "")
         }),
     }
